1ce4b01dfe724490b1e9f180613389b87a0fd88d
Gabriel Ribeiro <gabrielribeiroonline@gmail.com>
2019-11-06 16:03:39 -0200

    Adicionado feed de posts, pag. do post e paginacao

--- single.php
+++ single.php
@@ -11,15 +11,14 @@
           <!-- Estrutura do post -->
           <article>
 
-            <!-- Miniatura do post (imagem) -->
+            <!-- Título do post -->
+            <h2> <?php the_title(); ?> </h2>
+
+            <!-- Imagem do post -->
             <?php if(has_post_thumbnail()): ?>
-              <a href="<?php the_permalink(); ?>">
-                <?php the_post_thumbnail('large', array('class' => 'post_miniatura')); ?>
+                <?php the_post_thumbnail('large'); ?>
               </a>
             <?php endif; ?>
-            
-            <!-- Título do post -->
-            <h2><a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a></h2>
 
             <!-- Data, Autor e Categorias -->
             <p>
@@ -28,15 +27,14 @@
               <?php the_category(', '); ?>
             </p>
             
-            <!-- Exibir resumo do post -->
+            <!-- Exibir conteúdo do post -->
             <p>
-              <?php the_excerpt(); ?>
+              <?php the_content(); ?>
             </p>
 
-            <!-- Comentários / leia mais -->
+            <!-- Comentários -->
             <p>
-              <?php comments_number('0 comentários', '1 comentário', '% comentários'); ?> | 
-              <a href="<?php the_permalink(); ?>">LEIA MAIS</a>
+              <?php comments_number('0 comentários', '1 comentário', '% comentários'); ?>
             </p>
 
           </article>
@@ -45,14 +43,14 @@
       <?php endif; ?>
 
 
-      <!-- Páginação -->
+      <!-- Anterior e Próximo Post -->
       <div class="paginacao">
         <div class="pagina_anterior">
-          <?php previous_posts_link('< Página Anterior'); ?>
+          <?php previous_post_link(); ?>
         </div>
 
         <div class="proxima_pagina">
-          <?php next_posts_link('Próxima Página >') ?>
+          <?php next_post_link() ?>
         </div>
       </div>
 
